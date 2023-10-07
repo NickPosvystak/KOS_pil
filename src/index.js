2492292deda7e5a1c3ed5c1a2be1644b0168bff2
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-10-07 21:56:14 +0200

    add hero, btn, modal

--- src/index.js
+++ src/index.js
@@ -1,85 +1,30 @@
-import axios from 'axios';
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import SlimSelect from 'slim-select';
-import '/node_modules/slim-select/dist/slimselect.css';
-import './style.css';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import bodyScrollLock from 'body-scroll-lock';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_eSDqpwcopAalFSs8R7JouU8rzUDVV0WSNJfG9vqs9qv9Zb0rNWD8l7p05hdlIZdU';
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
-const refs = {
-  select: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('.cat-info'),
-  loader: document.querySelector('.loader'),
-  error: document.querySelector('.error'),
-};
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
 
-function slim() {
-  new SlimSelect({
-    select: refs.select,
+    // const scrollLockMethod = !isMenuOpen
+    //   ? 'disableBodyScroll'
+    //   : 'enableBodyScroll';
+    // bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    // bodyScrollLock.enableBodyScroll(document.body);
   });
-}
-
-refs.error.classList.add('is-hidden');
-refs.loader.classList.add('is-hidden');
-refs.catInfo.classList.add('is-hidden');
-
-fetchBreeds()
-  .then(data => {
-    refs.select.innerHTML = createList(data);
-    slim();
-
-    refs.loader.classList.remove('hidden');
-  })
-  .catch(onError);
-
-refs.select.addEventListener('change', selectedBreeds);
-
-function selectedBreeds(event) {
-  event.preventDefault();
-
-  refs.loader.classList.remove('is-hidden');
-  refs.loader.classList.add('loader');
-  refs.loader.textContent ="";
-
-  refs.catInfo.classList.add('is-hidden');
-
-  const breedId = event.currentTarget.value;
-
-  fetchCatByBreed(breedId)
-    .then(data => {
-      createMarkup(data);
-
-      refs.loader.classList.add('is-hidden');
-      refs.loader.classList.remove('loader');
-      refs.catInfo.classList.remove('is-hidden');
-      
-    })
-    .catch(onError);
-}
-
-function createList(arr) {
-  return arr
-    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-    .join('');
-}
-
-function createMarkup(data) {
-  const markupCard = data
-    .map(el => {
-      return `<img src="${el.url}" alt="${el.breeds[0].name}" width="400"/><li><h2>${el.breeds[0].name}</h2><p>${el.breeds[0].description}</p><h3>Temperament:</h3><p>${el.breeds[0].temperament}</p></li>`;
-    })
-    .join('');
-  refs.catInfo.innerHTML = markupCard;
-}
-
-function onError(error) {
-  refs.loader.classList.add('is-hidden');
-
-
-  Report.warning(
-    '',
-    "Oops! Something went wrong! Try reloading the page or select another cat breed'"
-  );
-}
+})();
