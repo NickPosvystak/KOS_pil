f9eaec24e9643cfacc71c008c80ae194b31de244
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-11-15 21:47:00 +0100

    update video, fix tablet size

---
+++ src/js/video.js
@@ -0,0 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
+player.setCurrentTime(currentTime);
