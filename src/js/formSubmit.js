b2b913513a6c1878bb232cc3ab1a1dff277e35a3
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-12-04 20:58:39 +0100

    fix

--- src/js/formSubmit.js
+++ src/js/formSubmit.js
@@ -1,35 +1,39 @@
 function sendEmail() {
-  let params = {
-    name: document.getElementById('form-name').value,
-    email: document.getElementById('form-email').value,
-    message: document.getElementById('form-message').value,
-  };
-  const serviceID = 'service_zd4sbq6';
-  const templateID = 'template_abxrfhl';
-  emailjs
-    .send(serviceID, templateID, params)
-    .then(res => {
-      (document.getElementById('form-name').value = ''),
-        (document.getElementById('form-email').value = ''),
-        (document.getElementById('form-message').value = ''),
+  // Get form input values
+  let userName = document.getElementById('form-name');
+  let userEmail = document.getElementById('form-email');
+  let messageField = document.getElementById('form-message');
+
+  // Check for empty fields
+  if (
+    userName.value === '' ||
+    userEmail.value === '' ||
+    messageField.value === ''
+  ) {
+    alert('Please fill in all fields before submitting the form!');
+  } else {
+    // If all fields are filled, proceed with sending the email
+    let params = {
+      name: userName.value,
+      email: userEmail.value,
+      message: messageField.value,
+    };
+
+    const serviceID = 'service_zd4sbq6';
+    const templateID = 'template_abxrfhl';
+
+    emailjs
+      .send(serviceID, templateID, params)
+      .then(res => {
+        // Clear form fields
+        userName.value = '';
+        userEmail.value = '';
+        messageField.value = '';
+
         console.log(res);
-      alert('This form has been successfully submitted!');
-    })
-    .catch(error => console.log(error.message));
+        alert('This form has been successfully submitted!');
+      })
+      .catch(error => console.log(error.message));
+  }
 }
 
-// submitForm.addEventListener('submit', event => {
-// //   event.preventDefault();
-
-//   if (userName.value === '' || userEmail.value === ''|| messageField.value === '') {
-//     alert('Ensure you input a value in both fields!');
-//   } else {
-//     alert('This form has been successfully submitted!');
-//   }
-//   console.log(
-//     `This form has a username of ${userName.value} and E-mail of ${userEmail.value}`
-//   );
-//   userName.value = '';
-//     userEmail.value = '';
-//     messageField.value = '';
-// });
